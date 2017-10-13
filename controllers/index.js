0cd4503d5b7e6f80853ab1a2b0c8c7022337c602
jamesw8 <jameswong8@gmail.com>
2017-10-13 18:31:48 -0400

    Added backend starter code with some routes

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 
-router.use('/alt', require('./alt'));
+// router.use('/alt', require('./alt'));
+// Index
 router.use('/', require('./home'));
-
+// Dashboard
+router.use('/dashboard', require('./dashboard'));
+// Login
+router.use('/login', require('./login'));
+// Logout
+router.use('/logout', require('./logout'));
 
 module.exports = router;
